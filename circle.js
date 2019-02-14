7dac7869f6ac5ca62b04ea14ea7ef8a4b8f7181e
Classified1 <32497472+Classified1@users.noreply.github.com>
2019-02-14 14:44:58 -0500

    Triangle_Strip to Triangle_Fan
    Also formatting.

--- circle.js
+++ circle.js
@@ -1,166 +1,166 @@
-		 var canvas = document.getElementById('my_Canvas');
-         var gl = canvas.getContext('experimental-webgl');
-		 
-		     var width = canvas.getAttribute("width"), height = canvas.getAttribute("height");
-    // Fullscreen if not set
-    if (width) {
-      gl.maxWidth = width;
-    }
-    if (height) {
-      gl.maxHeight = height;
-    }
+var canvas = document.getElementById('my_Canvas');
+var gl = canvas.getContext('experimental-webgl');
 
-         /*======= Defining and storing the geometry ======*/
+var width = canvas.getAttribute("width"), height = canvas.getAttribute("height");
+// Fullscreen if not set
+if (width) {
+    gl.maxWidth = width;
+}
+if (height) {
+    gl.maxHeight = height;
+}
+
+/*======= Defining and storing the geometry ======*/
 
 
-         // Create an empty buffer object
-         var vertex_buffer = gl.createBuffer();
-		 var vertices = []; 
-		 var vertCount = 2;
-		 
-		 for(var i=0.0; i<=360; i+=1){
-		var j = i * Math.PI / 180;
-		
-		var vert1 = [
-			Math.sin(j),
-			Math.cos(j),
-		];
-		
-		var vert2 = [
-			0,
-			0,
-		];
-		
-		vertices = vertices.concat(vert1);
-		vertices = vertices.concat(vert2);
-	}
-		var n = vertices.length/vertCount;
-	
+// Create an empty buffer object
+var vertex_buffer = gl.createBuffer();
+var vertices = [];
+var vertCount = 2;
 
-         // Bind appropriate array buffer to it
-         gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-      
-         // Pass the vertex data to the buffer
-         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+for (var i = 0.0; i <= 360; i += 1) {
+    var j = i * Math.PI / 180;
 
-         // Unbind the buffer
-         gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    var vert1 = [
+        Math.sin(j),
+        Math.cos(j),
+    ];
 
-         /*=================== Shaders ====================*/
+    /*var vert2 = [
+        0,
+        0,
+    ];*/
 
-function createShaders(c1, c2, c3, c4){
-	    // Vertex shader source code
-         var vertCode =
-            'attribute vec3 coordinates;' +
-            'void main(void) {' +
-               ' gl_Position = vec4(coordinates, 1.0);' +
-            '}';
+    vertices = vertices.concat(vert1);
+    //vertices = vertices.concat(vert2);
+}
+var n = vertices.length / vertCount;
 
-         // Create a vertex shader object
-         var vertShader = gl.createShader(gl.VERTEX_SHADER);
 
-         // Attach vertex shader source code
-         gl.shaderSource(vertShader, vertCode);
+// Bind appropriate array buffer to it
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 
-         // Compile the vertex shader
-         gl.compileShader(vertShader);
+// Pass the vertex data to the buffer
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
 
-         // Fragment shader source code
-         var fragCode =
-            'void main(void) {' +
-               'gl_FragColor = vec4('+c1+', '+c2+', '+c3+', '+c4+');' +
-            '}';
+// Unbind the buffer
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-         // Create fragment shader object
-         var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+/*=================== Shaders ====================*/
 
-         // Attach fragment shader source code
-         gl.shaderSource(fragShader, fragCode);
+function createShaders(c1, c2, c3, c4) {
+    // Vertex shader source code
+    var vertCode =
+       'attribute vec3 coordinates;' +
+       'void main(void) {' +
+          ' gl_Position = vec4(coordinates, 1.0);' +
+       '}';
 
-         // Compile the fragmentt shader
-         gl.compileShader(fragShader);
+    // Create a vertex shader object
+    var vertShader = gl.createShader(gl.VERTEX_SHADER);
 
-         // Create a shader program object to store
-         // the combined shader program
-         var shaderProgram = gl.createProgram();
+    // Attach vertex shader source code
+    gl.shaderSource(vertShader, vertCode);
 
-         // Attach a vertex shader
-         gl.attachShader(shaderProgram, vertShader);
+    // Compile the vertex shader
+    gl.compileShader(vertShader);
 
-         // Attach a fragment shader
-         gl.attachShader(shaderProgram, fragShader);
+    // Fragment shader source code
+    var fragCode =
+       'void main(void) {' +
+          'gl_FragColor = vec4(' + c1 + ', ' + c2 + ', ' + c3 + ', ' + c4 + ');' +
+       '}';
 
-         // Link both the programs
-         gl.linkProgram(shaderProgram);
+    // Create fragment shader object
+    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 
-         // Use the combined shader program object
-         gl.useProgram(shaderProgram);
-		 
-		/*======= Associating shaders to buffer objects ======*/
+    // Attach fragment shader source code
+    gl.shaderSource(fragShader, fragCode);
 
-         // Bind vertex buffer object
-         gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    // Compile the fragmentt shader
+    gl.compileShader(fragShader);
 
-         // Get the attribute location
-         var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+    // Create a shader program object to store
+    // the combined shader program
+    var shaderProgram = gl.createProgram();
 
-         // Point an attribute to the currently bound VBO
-         gl.vertexAttribPointer(coord, vertCount, gl.FLOAT, false, 0, 0);
+    // Attach a vertex shader
+    gl.attachShader(shaderProgram, vertShader);
 
-         // Enable the attribute
-         gl.enableVertexAttribArray(coord);
+    // Attach a fragment shader
+    gl.attachShader(shaderProgram, fragShader);
+
+    // Link both the programs
+    gl.linkProgram(shaderProgram);
+
+    // Use the combined shader program object
+    gl.useProgram(shaderProgram);
+
+    /*======= Associating shaders to buffer objects ======*/
+
+    // Bind vertex buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+    // Get the attribute location
+    var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+
+    // Point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, vertCount, gl.FLOAT, false, 0, 0);
+
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
 }
 
 
 
-		// Clear the canvas
-		gl.clearColor(0.5, 0.5, 0.5, 0.9);
+// Clear the canvas
+gl.clearColor(0.5, 0.5, 0.5, 0.9);
 
-		// Enable the depth test
-		gl.enable(gl.DEPTH_TEST);
-		
-		//Clear the color and depth buffer
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		
-		for(var i = 0; i < 10; i++){
-			createShaders(0,0,0,1);
-			drawBacteria();
-		}
+// Enable the depth test
+gl.enable(gl.DEPTH_TEST);
 
-		 drawMainCircle();
-		 
-		 
-		 function drawMainCircle(){
-				createShaders(0,0,0,0);
-				 // Set the view port
-				 gl.viewport(0,0,canvas.width,canvas.height);
+//Clear the color and depth buffer
+gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-				 // Draw the triangle
-				 gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
-				 
+for (var i = 0; i < 10; i++) {
+    createShaders(1, 0, 0, 1);
+    drawBacteria();
+}
 
-		 }
-		 
-		 function drawBacteria(){
-				//createShaders(0,0,0,1);
-				var coord = getRandomLoc();
-				
-				gl.viewport(coord[0]-50,coord[1]-50,100,100);
+drawMainCircle();
 
-				 // Draw the triangle
-				 gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
-			 //drawMainCircle();
-		 }
-		 
-		 function getRandomLoc(){
-			var rand = Math.random();
-			var angle = (rand*Math.PI*2);
-			var coord = [2];
-			console.log(angle);
-			coord[0] = 250+250*Math.cos(-angle);
-			console.log(coord[0]);
-			coord[1] = 250+250*Math.sin(-angle);
-			 return coord;
-		 }
 
-		 
+function drawMainCircle() {
+    createShaders(0, 0, 0, 0);
+    // Set the view port
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    // Draw the triangle
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
+
+
+}
+
+function drawBacteria() {
+    //createShaders(0,0,0,1);
+    var coord = getRandomLoc();
+
+    gl.viewport(coord[0] - 50, coord[1] - 50, 100, 100);
+
+    // Draw the triangle
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
+    //drawMainCircle();
+}
+
+function getRandomLoc() {
+    var rand = Math.random();
+    var angle = (rand * Math.PI * 2);
+    var coord = [2];
+    console.log(angle);
+    coord[0] = 250 + 250 * Math.cos(-angle);
+    console.log(coord[0]);
+    coord[1] = 250 + 250 * Math.sin(-angle);
+    return coord;
+}
+
+
